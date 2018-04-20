aafcc17e3f5bc35b3a943e649cf0a2c0da5cc6ac
Germán Santos <crashbito@gmail.com>
2018-04-20 18:54:44 -0500

    Binario ejemplo completo sin arreglos

--- binario.c
+++ binario.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <math.h>
 
 int menu();
 void dec2bin();
@@ -11,10 +13,10 @@ int main() {
         opcion = menu();
         switch(opcion){
             case 1:
-                bin2dec();
+                dec2bin();
                 break;
             case 2:
-                dec2bin();
+                bin2dec();
                 break;
             default:
                 break;
@@ -38,10 +40,35 @@ int menu(){
 }
 
 void dec2bin(){
+    int n, binario=0, residuo, i=1;
     printf("\n\nUsted seleccionó decimal 2 binario");
+    printf("Introduce un numero: ");
+    scanf("%d", &n);
+    
+    while(n != 0){
+        residuo = n % 2;
+        n = n / 2;
+        binario = binario + residuo *  i;
+        i = i * 10;
+    }
+    printf("El resultado es : %d", binario);
+    
 }
 
 void bin2dec(){
-    printf("\n\nUsted seleccionó binario 2 decimal");
+    int n, decimal=0, residuo, i=0;
+    printf("\n\nUsted seleccionó binario 2 decimal\n\n");
+    printf("Introduce un numero: ");
+    scanf("%d", &n);
+    
+    while(n != 0){
+        residuo = n % 10;
+        n = n / 10;
+        decimal = decimal + residuo * pow(2,i);
+        i = i + 1;
+    }
+
+    printf("El resultado es: %d", decimal);
+
 }
 
